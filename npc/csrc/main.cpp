38e1a5e0c5bfde4c63e6b76467df4e1978b83682
Zhong Huishan <zhonghuishan@tom.com>
2024-07-30 10:32:33 +0800

    ebreak pass

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,27 +1,140 @@
+// //================== 定义读取bin文件指令版 ===================================
+// #include <stdio.h> 
+// #include <stdlib.h> 
+// #include <assert.h>
+// // #include <stdint.h>
+// #include <time.h> 
+// #include "Vriscv_top.h"  
+// #include "verilated_vcd_c.h" 
+// // #include "Vriscv_top__Dpi.h"
+// // #include <verilated_dpi.h>
+
+// // //-------------------------------------------------------------------------------
+// Vriscv_top *top; 
+// vluint64_t cur_time;
+
+// double sc_time_stamp(){     //仿真时间戳函数
+//     return cur_time;  }
+
+// void time_inc(){
+//     cur_time++;}
+
+
+// //================== img =========================          //////////////////////////
+
+// //请求分配size个uint32_t所需的内存空间并返回指向这块内存空间的指针 memory
+
+// static long load_img();
+
+// //static char *img_file = "/home/zhong/ysyx-workbench/npc/image.bin";
+
+// uint32_t *memory = (uint32_t *) malloc ( 10 *sizeof(uint32_t) );    ////////分配size
+
+// uint32_t guest_to_host ( uint32_t addr ) {
+//     return addr - 0x80000000;
+// }
+
+// uint32_t pmem_read ( uint32_t *memory , uint32_t vaddr ){
+//     uint32_t paddr = guest_to_host( vaddr );
+//     return memory[paddr/4];         //paddr/4为序列号
+// }
+
+// //===================== main =======================
+// int main(int argc, char **argv){
+//  //   uint32_t *memory = init_mem(3);             /////////////////
+   
+//     Verilated::commandArgs(argc,argv); 
+//     Verilated::traceEverOn(true);               //Verilator开启VCD波形记录功能
+//     VerilatedVcdC *vcd = new VerilatedVcdC;     //用于管理Verilator的仿真上下文，包括仿真状态和配置参数
+//     srand((unsigned)time(NULL)); 
+//     top = new Vriscv_top("top"); 
+//     top->trace(vcd,0);                          //关联top和vcd，使得vcd可以记录top模块的信号变化
+//     vcd->open("wave.vcd");                      //打开名为 wave.vcd 的文件，准备开始记录仿真信号变化
+
+// //--------------------- load the img to memory ------------------------------
+//     long img_size = load_img();
+// //-----------------------复位到开启cpu_init ---------------------
+//     top -> clk = 0;
+//     top -> reset = 1;
+//     top -> eval(); 
+//     vcd -> dump(sc_time_stamp()); 
+//     time_inc();
+
+//     top -> clk = 1;
+//     top -> reset = 1;
+//     top -> eval(); 
+//     vcd -> dump(sc_time_stamp()); 
+//     time_inc();
+// //----------------------------------------------------
+//     //sc_time_stamp() 返回当前仿真时间
+//     while(sc_time_stamp()< 30 && !Verilated::gotFinish()){
+//         top -> reset = 0;
+//         top -> clk = 0;
+//         top -> Instr = pmem_read( memory , top -> PC );     /////////////////////
+//         top -> eval(); 
+//         vcd -> dump(sc_time_stamp()); 
+//         time_inc(); 
+
+//         top -> clk = 1;
+//         top -> eval(); 
+//         vcd -> dump(sc_time_stamp()); 
+//         time_inc(); 
+//     }
+//     free (memory);          /////////////////////
+
+//     top->final(); 
+//     vcd->close(); 
+//     delete top; 
+//     delete vcd; 
+//     return 0;
+// }
+
+// static long load_img() {      //加载一个镜像文件到内存中,并返回镜像文件的大小
+//     if (img_file == NULL) {     //检查是否有指定的镜像文件路径
+//     //Log("No image is given. Use the default build-in image.");  //没有外部指定的镜像文件，使用内置的镜像文件
+//     printf("No image is given. Use the default build-in image.\n");
+//     return 4096;              //默认的内置镜像大小为4096字节
+//     }
+//     FILE *fp = fopen(img_file, "rb");
+//     //Assert(fp, "Can not open '%s'", img_file);  //确保文件成功打开
+//     if( fp == NULL ) {
+//         printf("Can not open '%s'\n", img_file);
+//         assert(0);
+//     }
+//     fseek(fp, 0, SEEK_END);   //文件指针移动到文件末尾
+//     long size = ftell(fp);    //获取文件大小，字节数
+//     //Log("The image is %s, size = %ld", img_file, size);
+//     printf("The image is %s, size = %ld", img_file, size);
+//     fseek(fp, 0, SEEK_SET);   //文件指针移动到文件开头，准备开始读取文件内容
+//     //int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);    //fread从打开的文件中 读取size字节的数据，并将其写入guest_to_host(RESET_VECTOR)指向的内存地址中
+//     int ret = fread(memory, size, 1, fp);
+//     assert(ret == 1);         //检查 是否读取成功
+//     fclose(fp);
+//     return size;              //返回读取的镜像文件的大小
+// }
+
+
+
+
+
+
+
+//=======================定义数组存放指令版===================================
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <assert.h>
+#include <string.h>
 #include <time.h> 
 #include "Vriscv_top.h"  
 #include "verilated_vcd_c.h" 
+#include "Vriscv_top__Dpi.h"
+#include <verilated_dpi.h>
 
+#define MAX_SIM_TIME 30     //最大仿真时间
+#define INIT_MEM_SIZE 10    //初始化内存大小，初始化=0
 
-// #include "verilated.h"
-// //dpi-c
-// // #include "Vtop__Dpi.h"
-// #include <verilated_dpi.h>
-
-// // Difftest
-// #include <dlfcn.h>
-// //readline
-// #include <readline/readline.h>
-// #include <readline/history.h>
-// //system time
-// #include <sys/time.h>
-
-
-
-// //-------------------------------------------------------------------------------
+///# define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))     ////////////////////
+//--------------------------------------------------------------------------
 Vriscv_top *top; 
 vluint64_t cur_time;
 
@@ -31,7 +144,68 @@ double sc_time_stamp(){     //仿真时间戳函数
 void time_inc(){
     cur_time++;}
 
+//--------------------------- npc_trap ------------------------------------------
+int flag = 0;
+void npc_trap( int stop_signal ) {
+    flag = stop_signal;
+    printf("flag = %d\n",flag);     /////////
+}
+//---------------------------- ebreak ------------------------------------------
+
+extern "C" void ebreak( int stop_signal ){
+    printf("----------------------------------- ebreak ------------------------------------------- \n");
+    npc_trap(stop_signal);
+  // should not reach here
+    return;
+}
+//---------------------------- img -----------------------------------------
+// static const uint32_t img[] = {             //二进制指令
+//     0b00000000010100000000000010010011,     //addi x1 x0 5
+//     0b00000000000100000000000100010011,     //addi x2 x0 1
+//     0b00000000001000000000000100010011,     //addi x2 x0 2
+//     0b00000000010100001000000100010011      //addi x2 x15
+// };
+
+// static const uint32_t img[] = {             //十六进制非小端指令
+//     0x500093    ,     //addi x1 x0 5
+//     0x100113    ,     //addi x2 x0 1
+//     0x200113    ,     //addi x2 x0 2
+//     0x508113          //addi x2 x15
+// };
+
+static const uint32_t img[] = {             //十六进制小端序列指令
+    // 0x390005    ,     //addi x1 x0 5
+    // 0x311001    ,     //addi x2 x0 1
+    // 0x311002          //addi x2 x0 2
+       0x1c0c93    ,     //ADDI $25 $24 32'h00000001
+       0x100073          //ebreak
+};
+
+//---------------------------- init_mem  -----------------------------------------
+
+uint32_t *init_mem ( size_t size ) {
+    uint32_t *memory = (uint32_t *) malloc ( size * sizeof(uint32_t) );    ////////分配size
+    memset(memory, 0, size * sizeof(uint32_t));     //内存初始化为0
+
+    memcpy( memory , img , sizeof(img) );
+    if( memory==NULL ) {
+        exit(0);
+    }
+    return memory;
+}
+
+uint32_t guest_to_host ( uint32_t addr ) {
+    return addr - 0x80000000;
+}
+
+uint32_t pmem_read ( uint32_t *memory , uint32_t vaddr ){
+    uint32_t paddr = guest_to_host( vaddr );
+    return memory[paddr/4];         //paddr/4为序列号
+}
+
+//===================== main ======================================
 int main(int argc, char **argv){
+   
     Verilated::commandArgs(argc,argv); 
     Verilated::traceEverOn(true);               //Verilator开启VCD波形记录功能
     VerilatedVcdC *vcd = new VerilatedVcdC;     //用于管理Verilator的仿真上下文，包括仿真状态和配置参数
@@ -39,8 +213,10 @@ int main(int argc, char **argv){
     top = new Vriscv_top("top"); 
     top->trace(vcd,0);                          //关联top和vcd，使得vcd可以记录top模块的信号变化
     vcd->open("wave.vcd");                      //打开名为 wave.vcd 的文件，准备开始记录仿真信号变化
-    
-//-----------------------复位到开启---------------------
+//---------------------- init memory ---------------------------
+    uint32_t *memory = init_mem( INIT_MEM_SIZE );            //size为10，也只将10个uint32_t初始化为0
+
+//-----------------------复位到开启cpu_init ---------------------
     top -> clk = 0;
     top -> reset = 1;
     top -> eval(); 
@@ -52,15 +228,13 @@ int main(int argc, char **argv){
     top -> eval(); 
     vcd -> dump(sc_time_stamp()); 
     time_inc();
-//----------------------------------------------------
+//---------------------------------------------------------------
     //sc_time_stamp() 返回当前仿真时间
-    while(sc_time_stamp()< 50 && !Verilated::gotFinish()){
-        // int a = rand() & 1; 
-        // int b = rand() & 1; 
-        // top -> a = a; 
-        // top -> b = b; 
+    while(sc_time_stamp()< MAX_SIM_TIME && !Verilated::gotFinish()){
+
         top -> reset = 0;
         top -> clk = 0;
+        top -> Instr = pmem_read( memory , top -> PC );     /////////////////////
         top -> eval(); 
         vcd -> dump(sc_time_stamp()); 
         time_inc(); 
@@ -68,127 +242,29 @@ int main(int argc, char **argv){
         top -> clk = 1;
         top -> eval(); 
         vcd -> dump(sc_time_stamp()); 
-
         time_inc(); 
+
+        if( flag == 1 ){
+            printf("Simulation finished \n");
+            flag == 0;
+            free (memory);          /////////////////////
+            top->final();                       //////////////////////
+            vcd->close(); 
+            delete top; 
+            delete vcd; 
+            return 0;
+        }
     }
-        
+    free (memory);          /////////////////////
+
     top->final(); 
     vcd->close(); 
     delete top; 
     delete vcd; 
-    return 0;}
+    return 0;
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //-------------------------------------------------------------------------------
-
-// //================= Environment ===============
-// VerilatedContext* contextp;
-// Vriscv_top *top; 
- 
-// VerilatedVcdC* tfp;
-// vluint64_t main_time = 0;  //initial 仿真时间
-// double sc_time_stamp()
-// {
-// 	return main_time;
-// }
- 
-// uint64_t ref_regs[33];
- 
-// void hit_exit(int status) {}
-
-// //================= Memory ====================
-// addr_t img_size = 0;
-// uint8_t pmem[10485760] = {0};
- 
-// uint8_t* cpu2mem(addr_t addr) {}
- 
-// void pmem_init() {
-//   char image_path[] = "/home/zhong/ysyx-workbench/npc/image.bin";
-// }
-
-// //================= Exec =====================
-// void cpu_init() {
-//   //cpu_gpr[32] = CONFIG_MBASE;
-//   top -> clk = 0;
-//   top -> rst_n = 0;
-//   top -> eval();
-//   tfp->dump(main_time);
-//   main_time ++;
-//   top -> clk = 1;
-//   top -> rst_n = 0;
-//   top -> eval();
-//   tfp->dump(main_time);
-//   main_time ++;
-//   top -> rst_n = 1;
-// }
-
-
-// void exec_once(VerilatedVcdC* tfp) {
-//   top->clk = 0;
-//   //printf("======clk shoule be 0 now %d\n",top->clk);
-//   // top->mem_inst = pmem_read(top->mem_addr);
-//   // printf("excute addr:0x%08lx inst:0x%08x\n",top->mem_addr,top->mem_inst);
-//   top->eval();
-//   tfp->dump(main_time);
-//   main_time ++;
-//   top->clk = 1;
-//   //printf("======clk should be 1 now %d\n",top->clk); 
-//   top->eval(); 
-// 	tfp->dump(main_time);
-//   main_time ++;
-// }
- 
-// void cpu_exec(uint64_t n) {
-//   for(int i; i < n; i++){
-//       exec_once(tfp);
-//       #ifdef CONFIG_DIFFTEST
-//         difftest_exec_once();
-//       #endif
-//   }
-// }
-
-
-
-// //============ Main ============
-// int main(int argc, char** argv, char** env) {
-//   contextp = new VerilatedContext;
-//   contextp->commandArgs(argc, argv);
-//   top = new Vtop{contextp};
-//   //VCD波形设置  start
-//   Verilated::traceEverOn(true);
-//   tfp = new VerilatedVcdC;
-//   top->trace(tfp, 0);
-//   tfp->open("wave.vcd");
-//   //VCD波形设置  end
-//   //initial data
-//   pmem_init();
-//   cpu_init();
- 
-//   #ifdef CONFIG_DIFFTEST
-//     init_difftest();
-//   #endif
- 
-// //   sdb_mainloop();
- 
-//   return 0;
-// }
 
